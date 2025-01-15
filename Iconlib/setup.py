cef88101e92cc4177bfbad30c8af9def3e57d15d
VIKHRAM S <146576573+Vikhram-S@users.noreply.github.com>
2025-01-15 16:52:34 +0530

    Update setup.py

--- Iconlib/setup.py
+++ Iconlib/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 # Reading the README file for the long description
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="iconlib", 
@@ -10,21 +10,20 @@ setup(
     author="Vikhram S",  
     author_email="vikhrams@saveetha.ac.in",  
     description="A Python library for exploring the Constitution of India.",
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/Vikhram-S/Iconlib",  
     packages=find_packages(),  
     include_package_data=True,  
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Apache License 2.0",  
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Intended Audience :: Civil Enthusiasts",
         "Topic :: Software Development :: Libraries",
     ],
     python_requires=">=3.7", 
-
     extras_require={
         "dev": ["pytest", "flake8"],  
     },
