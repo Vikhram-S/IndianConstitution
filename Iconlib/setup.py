388d44e4ac16b768363231ea6021b49cf1aec640
VIKHRAM S <146576573+Vikhram-S@users.noreply.github.com>
2025-01-15 17:24:08 +0530

    Update setup.py

--- Iconlib/setup.py
+++ Iconlib/setup.py
@@ -1,45 +1,45 @@
 from setuptools import setup, find_packages
-import urllib.request
+import os
 
-# Reading the README file from GitHub URL
-url = "https://raw.githubusercontent.com/Vikhram-S/Iconlib/main/README.md"
-try:
-    with urllib.request.urlopen(url) as response:
-        long_description = response.read().decode('utf-8')
-except Exception as e:
-    long_description = "A Python library for exploring the Constitution of India."
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="iconlib",
-    version="1.0.0",
-    author="Vikhram S",
-    author_email="vikhrams@saveetha.ac.in",
-    description="A Python library for exploring the Constitution of India.",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url="https://github.com/Vikhram-S/Iconlib",
+    name='iconlib',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Civil Enthusiasts",
-        "Topic :: Software Development :: Libraries",
+    install_requires=[
+        # Add required packages here if necessary, e.g., 'requests', 'numpy'
     ],
-    python_requires=">=3.7",
+    author='Vikhram S',
+    author_email='vikhrams@saveetha.ac.in',
+    description='A Python library for exploring the Constitution of India.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Vikhram-S/Iconlib',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Civil Enthusiasts',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.7',
     extras_require={
-        "dev": ["pytest", "flake8"],
+        'dev': ['pytest', 'flake8'],
     },
     entry_points={
-        "console_scripts": [
-            "iconlib-cli=iconlib.__main__:main",
+        'console_scripts': [
+            'iconlib-cli=iconlib.__main__:main',
         ],
     },
     project_urls={
-        "Documentation": "https://github.com/Vikhram-S/Iconlib/blob/main/README.md",
-        "Source": "https://github.com/Vikhram-S/Iconlib",
-        "Tracker": "https://github.com/Vikhram-S/Iconlib/issues",
+        'Documentation': 'https://github.com/Vikhram-S/Iconlib/blob/main/README.md',
+        'Source Repository': 'https://github.com/Vikhram-S/Iconlib',
+        'Issue Tracker': 'https://github.com/Vikhram-S/Iconlib/issues',
     },
 )
